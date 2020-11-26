50da5e45cf1f944e9793f74cbf6185df48af9954
Ugur Tigu <ugurtigu@gmail.com>
2020-11-26 17:26:00 +0100

    no database, no id bug fixed

--- src/pages/ideas/Ideas.js
+++ src/pages/ideas/Ideas.js
@@ -10,25 +10,8 @@ import {
 } from './Ideas.styled';
 import { data } from './IdeasData';
 import { Link } from 'react-router-dom';
+import { string_to_slug } from './../../utils/stringToSlug';
 
-const string_to_slug = (str) => {
-  str = str.replace(/^\s+|\s+$/g, ''); // trim
-  str = str.toLowerCase();
-
-  // remove accents, swap ñ for n, etc
-  const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
-  const to = 'aaaaeeeeiiiioooouuuunc------';
-  for (var i = 0, l = from.length; i < l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-  }
-
-  str = str
-    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-    .replace(/\s+/g, '-') // collapse whitespace and replace by -
-    .replace(/-+/g, '-'); // collapse dashes
-
-  return str;
-};
 export const Ideas = ({ theme }) => {
   return (
     <Container theme={theme}>
--- src/pages/ideas/IdeasData.js
+++ src/pages/ideas/IdeasData.js
@@ -1,50 +1,52 @@
 import { nanoid } from 'nanoid';
-
+const uid = function () {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
 export const data = [
   {
-    id: nanoid(4),
+    id: '0',
     title: 'Similar post to this! Lorem, ipsum 1.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
     postCategory: 'article',
   },
   {
-    id: nanoid(4),
+    id: '1',
     title: 'Similar post to this! Lorem, ipsum 2.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
     postCategory: 'article',
   },
   {
-    id: nanoid(4),
+    id: '2',
     title: 'Similar post to this! Lorem, ipsum 3.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
     postCategory: 'article',
   },
   {
-    id: nanoid(4),
+    id: '3',
     title: 'Similar post to this! Lorem, ipsum 4.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
     postCategory: 'article',
   },
   {
-    id: nanoid(4),
+    id: '4',
     title: 'Similar post to this! Lorem, ipsum 5.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
     postCategory: 'article',
   },
   {
-    id: nanoid(4),
+    id: '5',
     title: 'Similar post to this! Lorem, ipsum 6.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
     postCategory: 'article',
   },
   {
-    id: nanoid(4),
+    id: '6',
     title: 'Similar post to this! Lorem, ipsum 8.',
     date: '2020-11-26',
     tags: ['it', 'digital', 'react'],
