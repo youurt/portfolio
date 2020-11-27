885d8d9d2811789ffcc397da5be89feb932b0756
Ugur Tigu <ugurtigu@gmail.com>
2020-11-27 09:40:31 +0100

    changes footer grid layout media small

--- src/components/footer/Footer.styled.js
+++ src/components/footer/Footer.styled.js
@@ -4,16 +4,27 @@ export const FooterGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 3.5rem;
+  @media screen and (max-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr;
+    text-align: center;
+  }
 `;
 
 export const FooterFirst = styled.div`
   text-align: left;
+  @media screen and (max-width: 768px) {
+    text-align: center;
+  }
 `;
 export const FooterCenter = styled.div`
   text-align: center;
 `;
 export const FooterLast = styled.div`
   text-align: right;
+  @media screen and (max-width: 768px) {
+    text-align: center;
+  }
 `;
 
 export const Big = styled.div`
--- src/pages/app/App.styled.js
+++ src/pages/app/App.styled.js
@@ -52,16 +52,11 @@ export const GlobalTheme = createGlobalStyle`
 
   h2{
     color: ${({ theme }) => theme.primaryHover};
-    /* line-height: 1.25em; */
     font-size: 2.8em;
     font-weight: 700;
     margin-block-start: 0.67em;
     margin-block-end: 0.67em;
-    /* display: block; */
-    /* margin-block-start: 0.67em;
-    margin-block-end: 0.67em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px; */
+
   } 
   
   h3, h4 {
