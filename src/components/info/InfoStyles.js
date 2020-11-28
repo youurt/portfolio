eaa17a3e7b12a3a09b5639aac171a54fc3220249
Ugur Tigu <ugurtigu@gmail.com>
2020-11-28 16:56:36 +0100

    more adjustment

--- src/components/info/InfoStyles.js
+++ src/components/info/InfoStyles.js
@@ -16,7 +16,7 @@ const LeftInfoStyle = styled.div`
   margin-block-end: 1em;
   z-index: 30;
   @media (max-width: 768px) {
-    left: -4.4rem;
+    left: -4.8rem;
     font-size: 1em;
     font-weight: 500;
   }
@@ -38,7 +38,7 @@ const RightInfoStyle = styled.div`
   margin-block-end: 1em;
   text-decoration: none;
   @media (max-width: 768px) {
-    right: -4.4rem;
+    right: -4.8rem;
     font-size: 1em;
     font-weight: 500;
   }
