90f80d5413243581dc41c0e1ad642349d8063794
Ugur Tigu <ugurtigu@gmail.com>
2020-12-31 18:16:11 +0100

    changes mail

--- src/components/info/RightInfo.js
+++ src/components/info/RightInfo.js
@@ -5,7 +5,7 @@ const RightInfo = () => {
     <>
       <RightInfoStyle>
         <a href="mailto:ugurtigu@gmail.com" target="_blank">
-          info@ugurtigu.com
+          ugurtigu@gmail.com
         </a>
       </RightInfoStyle>
     </>
