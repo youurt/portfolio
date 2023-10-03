278eceb7826092ce48f644302f02068026ed4948
youurt <ugurtigu@gmail.com>
2023-10-03 12:48:28 +0200

    adjust footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Toggle } from './../../components';
+import React from "react";
+import { Toggle } from "./../../components";
 import {
-  FooterGrid,
-  FooterFirst,
-  FooterCenter,
-  FooterLast,
   Big,
-} from './Footer.styled';
+  FooterCenter,
+  FooterFirst,
+  FooterGrid,
+  FooterLast,
+} from "./Footer.styled";
 
 const Footer = ({ theme, toggleTheme }) => {
   return (
@@ -14,7 +14,7 @@ const Footer = ({ theme, toggleTheme }) => {
       <FooterFirst>
         <p>
           <strong>Ugur Tigu</strong> <br />
-          Developer + designer, passionate about data.
+          Developer.
         </p>
       </FooterFirst>
       <FooterCenter>
@@ -30,17 +30,13 @@ const Footer = ({ theme, toggleTheme }) => {
           <a href="https://github.com/youurt" target="_blank">
             GitHub
           </a>
-          ,{' '}
+          ,{" "}
           <a href="https://www.linkedin.com/in/youurt/" target="_blank">
             LinkedIn
           </a>
           , <br />
           <a href="https://codepen.io/youurt" target="_blank">
             Codepen
-          </a>
-          ,
-          <a href="https://twitter.com/_datacat_" target="_blank">
-            Twitter
           </a>
         </p>
       </FooterLast>
